f6a5fc60a4a83c3656e6a07f03bad45e432ef506
Igor <nhdigor@gmail.com>
2023-09-10 15:53:58 +0300

    Backend / 3. Стандартные модули / 2. Модуль OS

---
+++ refs/os.js
@@ -0,0 +1,9 @@
+const os = require('os')
+
+// console.log(os.platform()) // win32
+// console.log(os.arch()) // x64
+// console.log(os.cpus())
+// console.log(os.freemem())
+// console.log(os.totalmem())
+// console.log(os.uptime())
+console.log(os.homedir())
