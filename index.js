76b318079a65e3b71f843b226e191445e32dcb44
Igor <nhdigor@gmail.com>
2023-09-14 02:39:12 +0300

    Backend / 4. Web-сервер / 5. Работа с Express.js

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-const http = require('http')
+// const http = require('http')
+const express = require('express')
 const chalk = require('chalk')
-const fsPro = require('fs/promises')
+// const fsPro = require('fs/promises')
 const path = require('path')
 const { addNote } = require('./notes.controller')
-const { title } = require('process')
 
 const port = 3000
 const basePath = path.join(__dirname, 'pages')
 
-const server = http.createServer(async (req, res) => {
-  if (req.method === 'GET') {
-    const content = await fsPro.readFile(path.join(basePath, 'index.html'))
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-    })
-    res.end(content)
-  } else if (req.method === 'POST') {
-    const body = []
-    req.on('data', (data) => {
-      body.push(Buffer.from(data))
-    })
-    req.on('end', () => {
-      const title = body.toString().split('=')[1].replaceAll('+', ' ')
-      addNote(title)
-      res.end(`Title = ${title}`)
-    })
-    res.writeHead(200, {
-      'Content-Type': 'text/plain, charset=utf-8',
-    })
-  }
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(basePath, 'index.html'))
 })
 
-server.listen(port, () => {
+app.post('/', async (req, res) => {
+  await addNote(req.body.title)
+  res.sendFile(path.join(basePath, 'index.html'))
+})
+
+app.listen(port, () => {
   console.log(chalk.green(`Server has been started on port: ${port} ...`))
 })
