2df9527015b32f2fef643de5c709bda5a92be8f3
Igor <nhdigor@gmail.com>
2023-09-13 13:25:56 +0300

    Backend / 4. Web-сервер / 4. Обработка POST-запроса

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http')
 const chalk = require('chalk')
 const fsPro = require('fs/promises')
 const path = require('path')
+const { addNote } = require('./notes.controller')
+const { title } = require('process')
 
 const port = 3000
 const basePath = path.join(__dirname, 'pages')
@@ -13,6 +15,19 @@ const server = http.createServer(async (req, res) => {
       'Content-Type': 'text/html',
     })
     res.end(content)
+  } else if (req.method === 'POST') {
+    const body = []
+    req.on('data', (data) => {
+      body.push(Buffer.from(data))
+    })
+    req.on('end', () => {
+      const title = body.toString().split('=')[1].replaceAll('+', ' ')
+      addNote(title)
+      res.end(`Title = ${title}`)
+    })
+    res.writeHead(200, {
+      'Content-Type': 'text/plain, charset=utf-8',
+    })
   }
 })
 
